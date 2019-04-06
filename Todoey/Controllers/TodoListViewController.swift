13e4faf29e403c6fe6bbff093815f1aae099edb4
novyk <novyk@novyks-macbook-pro.local>
2019-04-06 10:05:18 +0200

    Convert to MVC design pattern

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -8,10 +8,19 @@
 
 import UIKit
 
+
+
+
+
+
+
 class TodoListViewController: UITableViewController {
 
+
     
-    var itemArray = ["Find Mike","Buy Eggs","Destroy City"]
+    
+    
+    var itemArray = [Item]()
     
     let defaults = UserDefaults.standard
     
@@ -19,9 +28,28 @@ class TodoListViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let newItem = Item()
+        newItem.title = "Find Javi"
+        itemArray.append(newItem)
+        let newItem2 = Item()
+        newItem2.title = "Find Javi"
+        itemArray.append(newItem2)
+        let newItem3 = Item()
+        newItem3.title = "Find Javi"
+        itemArray.append(newItem3)
+ 
+        for kkk in 1...15 {
+            let newItem4 = Item()
+            newItem4.title = "Find Javi \(kkk)"
+            itemArray.append(newItem4)
+
+            
+        }
+        
         // Do any additional setup after loading the view, typically from a nib.
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
             itemArray = items
         }
     }
@@ -32,8 +60,22 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
         
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+        /*
+        if item.done {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+        */
         return cell
         
         
@@ -54,22 +96,12 @@ class TodoListViewController: UITableViewController {
     // MARK - TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(itemArray[indexPath.row])
+        print(itemArray[indexPath.row].title)
         
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
-        
-        //tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
-        
-        
-        //tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark ? .none : .checkmark
-        
-        
         
     }
     
@@ -82,16 +114,18 @@ class TodoListViewController: UITableViewController {
         
         var textField = UITextField()
         
-        
-        
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             // what will happen once the user clicks the Add Item button on our UIAlert
         
+            
+            let newItem = Item()
+            newItem.title = textField.text!
+            
             print(textField.text!)
             
-            self.itemArray.append(textField.text!)
+            self.itemArray.append(newItem)
             
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by novyk on 5/4/19.
+//  Copyright © 2019 novyk. All rights reserved.
+//
+
+import Foundation
+
+
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
